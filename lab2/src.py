9a81221c918235cb39b8e29cee7ea3a6db599e8c
kingsj0405 <kingsj0405@naver.com>
2018-07-14 15:52:17 +0900

    Lab2 | Use placeholder

--- lab2/src.py
+++ lab2/src.py
@@ -1,18 +1,17 @@
 import tensorflow as tf
 
-# Set the data
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
-
 # Set the variable
 W = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
+# Make placeholder
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
 
 # hypothesis function
-hypothesis = x_train * W + b
+hypothesis = X * W + b
 
 # cost/lost function
-cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
@@ -25,6 +24,8 @@ sess.run(tf.global_variables_initializer())
 
 # Fit the line
 for step in range(2001):
-    sess.run(train)
+    cost_val, W_val, b_val, _ = sess.run([cost, W, b, train],
+                                         feed_dict={X: [1, 2, 3, 4, 5],
+                                                    Y: [2.1, 3.1, 4.1, 5.1, 6.1]})
     if step % 20 == 0:
-        print(step, sess.run(cost), sess.run(W), sess.run(b))
+        print(step, cost_val, W_val, b_val)
