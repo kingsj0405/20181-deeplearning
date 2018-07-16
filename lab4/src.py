4e80eda4bd043a182dec2e2908d347531223f373
kingsj0405 <kingsj0405@naver.com>
2018-07-16 11:43:19 +0900

    Lab4 | Improve by using matrix multiplication

--- lab4/src.py
+++ lab4/src.py
@@ -1,26 +1,21 @@
 import tensorflow as tf
 import numpy as np
 
-raw_data = [
+data = np.array([
     [73, 80, 75, 152],
     [93, 88, 93, 185],
     [89, 91, 90, 180],
     [96, 98, 100, 196],
     [73, 66, 70, 142]
-]
-data = np.array(raw_data)
-data = data.transpose()
+])
+x_data, y_data = np.hsplit(data, [3])
 
-x1 = tf.placeholder(tf.float32)
-x2 = tf.placeholder(tf.float32)
-x3 = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
 
-w1 = tf.Variable(tf.random_normal([1]), name='weight1')
-w2 = tf.Variable(tf.random_normal([1]), name='weight2')
-w3 = tf.Variable(tf.random_normal([1]), name='weight3')
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
-hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
+hypothesis = tf.matmul(X, W) + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
@@ -30,7 +25,7 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 for step in range(2001):
     cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
-                                   feed_dict={x1: data[0], x2: data[1], x3: data[2], Y: data[3]})
+                                   feed_dict={X: x_data, Y: y_data})
 
     if step % 10 == 0:
         print(step, "Cost: ", cost_val, "\nPrediction: ", hy_val, "\n")
