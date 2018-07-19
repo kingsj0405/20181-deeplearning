609b3bfba5237951c2d88329d195fe2bf976c15b
kingsj0405 <kingsj0405@naver.com>
2018-07-19 18:49:36 +0900

    Lab9 | Tensorflow log for TensorBoard

--- lab9/xor.py
+++ lab9/xor.py
@@ -7,33 +7,64 @@ y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
-W1 = tf.Variable(tf.random_normal([2, 10]), name='weight1')
-b1 = tf.Variable(tf.random_normal([10]), name='bias1')
-layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+with tf.name_scope("layer1"):
+    W1 = tf.Variable(tf.random_normal([2, 10]), name='weight1')
+    b1 = tf.Variable(tf.random_normal([10]), name='bias1')
+    layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 
-W2 = tf.Variable(tf.random_normal([10, 10]), name='weight2')
-b2 = tf.Variable(tf.random_normal([10]), name='bias2')
-layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+    w1_hist = tf.summary.histogram("weights1", W1)
+    b1_hist = tf.summary.histogram("biases1", b1)
+    layer1_hist = tf.summary.histogram("layer1", layer1)
 
-W3 = tf.Variable(tf.random_normal([10, 10]), name='weight3')
-b3 = tf.Variable(tf.random_normal([10]), name='bias3')
-layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
+with tf.name_scope("layer2"):
+    W2 = tf.Variable(tf.random_normal([10, 10]), name='weight2')
+    b2 = tf.Variable(tf.random_normal([10]), name='bias2')
+    layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
-W4 = tf.Variable(tf.random_normal([10, 1]), name='weight4')
-b4 = tf.Variable(tf.random_normal([1]), name='bias4')
-hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+    w2_hist = tf.summary.histogram("weights2", W2)
+    b2_hist = tf.summary.histogram("biases2", b2)
+    layer2_hist = tf.summary.histogram("layer2", layer2)
 
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1-hypothesis))
-train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+with tf.name_scope("layer3"):
+    W3 = tf.Variable(tf.random_normal([10, 10]), name='weight3')
+    b3 = tf.Variable(tf.random_normal([10]), name='bias3')
+    layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
+
+    w3_hist = tf.summary.histogram("weights3", W3)
+    b3_hist = tf.summary.histogram("biases3", b3)
+    layer3_hist = tf.summary.histogram("layer3", layer3)
+
+with tf.name_scope("layer4"):
+    W4 = tf.Variable(tf.random_normal([10, 1]), name='weight4')
+    b4 = tf.Variable(tf.random_normal([1]), name='bias4')
+    hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+
+    w4_hist = tf.summary.histogram("weights4", W4)
+    b4_hist = tf.summary.histogram("biases4", b4)
+    hypothesis_hist = tf.summary.histogram("hypothesis", hypothesis)
+
+with tf.name_scope("cost"):
+    cost = -tf.reduce_mean(Y * tf.log(hypothesis) +
+                           (1 - Y) * tf.log(1-hypothesis))
+    cost_summ = tf.summary.scalar("cost", cost)
+
+with tf.name_scope("train"):
+    train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 
 predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 
 with tf.Session() as sess:
+    # tensorboard --logdir=./logs/xor_logs
+    summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs/xor_logs_r0_01")
+    writer.add_graph(sess.graph)
+
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        s, _ = sess.run([summary, train], feed_dict={X: x_data, Y: y_data})
+        writer.add_summary(s, global_step=step)
         if step % 100 == 0:
             print(step,
                   sess.run(cost, feed_dict={X: x_data, Y: y_data}))
