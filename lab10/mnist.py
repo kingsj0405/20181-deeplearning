6436399ad936ba18e2308deed1233a2355830e20
kingsj0405 <kingsj0405@naver.com>
2018-07-20 11:03:37 +0900

    Lab10 | Add dropout
    Accuracy: 0.9797

--- lab10/mnist.py
+++ lab10/mnist.py
@@ -23,16 +23,21 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # input place holders
 X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
 
 # weights & bias for nn layers
 W1, b1, _L1 = get_layer(1, 784, 512, X)
 L1 = tf.nn.relu(_L1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 W2, b2, _L2 = get_layer(2, 512, 512, L1)
 L2 = tf.nn.relu(_L2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 W3, b3, _L3 = get_layer(3, 512, 512, L2)
 L3 = tf.nn.relu(_L3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 W4, b4, _L4 = get_layer(4, 512, 512, L3)
 L4 = tf.nn.relu(_L4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 W5, b5, _L5 = get_layer(5, 512, 10, L4)
 hypothesis = _L5
 
@@ -51,7 +56,7 @@ for epoch in range(training_epochs):
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        feed_dict = {X: batch_xs, Y: batch_ys}
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
         c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
         avg_cost += c / total_batch
 
@@ -63,4 +68,4 @@ print('Learning Finished!')
 correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print('Accuracy:', sess.run(accuracy,
-                            feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+                            feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1.0}))
