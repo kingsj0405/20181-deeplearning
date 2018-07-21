9845faf384088077ae36174625f7c722b83dbcbd
kingsj0405 <kingsj0405@naver.com>
2018-07-22 02:42:23 +0900

    Lab11 | Multiple filter and Padding for same size

--- lab11/cnn.py
+++ lab11/cnn.py
@@ -11,16 +11,16 @@ print("image.shape", image.shape)
 # plt.imshow(image.reshape(3, 3), cmap='Greys')
 # plt.show()
 
-weight = tf.constant([[[[1.]], [[1.]]],
-                      [[[1.]], [[1.]]]])
+weight = tf.constant([[[[1., 10., -1.]], [[1., 10., -1.]]],
+                      [[[1., 10., -1.]], [[1., 10., -1.]]]])
 print("weight.shape", weight)
-conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='VALID')
+conv2d = tf.nn.conv2d(image, weight, strides=[1, 1, 1, 1], padding='SAME')
 conv2d_img = conv2d.eval()
 print("conv2d_img.shape", conv2d_img.shape)
 conv2d_img = np.swapaxes(conv2d_img, 0, 3)
 # Output
 for i, one_img in enumerate(conv2d_img):
-    print(i, ": ", one_img.reshape(2, 2))
+    print(i, ": ", one_img.reshape(3, 3))
     plt.subplot(1, 2, i+1)
-    plt.imshow(one_img.reshape(2, 2), cmap='gray')
+    plt.imshow(one_img.reshape(3, 3), cmap='gray')
 plt.show()
