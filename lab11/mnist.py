e364550769c7088cc3221c9b6f0311a86d8a3911
kingsj0405 <kingsj0405@naver.com>
2018-07-23 20:14:07 +0900

    Lab 11 | Use 8 models ensemble
    0 Accuracy: 0.9934
    1 Accuracy: 0.9926
    2 Accuracy: 0.9948
    3 Accuracy: 0.9931
    4 Accuracy: 0.9942
    5 Accuracy: 0.9929
    6 Accuracy: 0.9937
    7 Accuracy: 0.9945
    Ensemble accuracy: 0.9954

--- lab11/mnist.py
+++ lab11/mnist.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -75,28 +76,47 @@ class Model:
 # constant
 training_epochs = 15
 batch_size = 128
+num_models = 8
 
 # Read data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # Initialize tensorflow session
 sess = tf.Session()
-m1 = Model(sess, "m1")
+models = []
+for m in range(num_models):
+    models.append(Model(sess, "model" + str(m)))
 
 sess.run(tf.global_variables_initializer())
 
 # Train
 print("Learning started.")
 for epoch in range(training_epochs):
-    avg_cost = 0
+    avg_cost_list = np.zeros(num_models)
     total_batch = int(mnist.train.num_examples / batch_size)
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        c, _ = m1.train(batch_xs, batch_ys)
-        avg_cost += c / total_batch
+
+        for m_idx, m in enumerate(models):
+            c, _ = m.train(batch_xs, batch_ys)
+            avg_cost_list[m_idx] += c / total_batch
     print('Epoch: ', '%04d' % (epoch + 1),
-          'cost = ', '{:.9f}'.format(avg_cost))
+          'cost = ', avg_cost_list)
 print("Learning Finished!")
 
 # Test
-print('Accuracy:', m1.get_accuracy(mnist.test.images, mnist.test.labels))
+test_size = len(mnist.test.labels)
+predictions = np.zeros([test_size, 10])
+for m_idx, m in enumerate(models):
+    print(m_idx, 'Accuracy:', m.get_accuracy(
+        mnist.test.images, mnist.test.labels
+    ))
+    p = m.predict(mnist.test.images)
+    predictions += p
+ensemble_correct_prediction = tf.equal(
+    tf.argmax(predictions, 1), tf.argmax(mnist.test.labels, 1)
+)
+ensemble_accuracy = tf.reduce_mean(
+    tf.cast(ensemble_correct_prediction, tf.float32)
+)
+print('Ensemble accuracy:', sess.run(ensemble_accuracy))
