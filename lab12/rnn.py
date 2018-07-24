22ed5b20fd18afb9a502d9c3b353f3f8a9258720
kingsj0405 <kingsj0405@naver.com>
2018-07-24 17:09:52 +0900

    Lab 12 | Deep RNN for long sentence

--- lab12/rnn.py
+++ lab12/rnn.py
@@ -3,8 +3,10 @@ import numpy as np
 
 
 # Make data
-# sample = " if you want you"  # work very well
-sample = " Mr. and Mrs. Dursley of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much."
+# sample = " if you want you"
+# sample = " Mr. and Mrs. Dursley of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much."
+# sample = " 안녕하세요"
+sample = " 프리벳가 4번지에 살고 있는 더즐리 부부는 자신들이 정상적이라는 것을 아주 자랑스럽게 여기는 사람들이었다."
 idx2char = list(set(sample))
 char2idx = {c: i for i, c in enumerate(idx2char)}
 sample_idx = [char2idx[c] for c in sample]
@@ -18,7 +20,7 @@ hidden_size = len(char2idx)
 num_classes = len(char2idx)
 batch_size = 1
 sequence_length = len(sample) - 1
-learning_rate = 1e-2
+learning_rate = 1e-1
 
 # placeholder
 X = tf.placeholder(tf.int32, [None, sequence_length])
@@ -27,9 +29,17 @@ Y = tf.placeholder(tf.int32, [None, sequence_length])
 
 # RNN model
 cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+cell = tf.contrib.rnn.MultiRNNCell([cell] * 2, state_is_tuple=True)
 initial_state = cell.zero_state(batch_size, tf.float32)
 outputs, _states = tf.nn.dynamic_rnn(
     cell, X_one_hot, initial_state=initial_state, dtype=tf.float32)
+
+# softmax
+X_for_softmax = tf.reshape(outputs, [-1, hidden_size])
+softmax_w = tf.get_variable("softmax_w", [hidden_size, num_classes])
+softmax_b = tf.get_variable("softmax_b", [num_classes])
+outputs = tf.matmul(X_for_softmax, softmax_w) + softmax_b
+outputs = tf.reshape(outputs, [batch_size, sequence_length, num_classes])
 
 # cost/loss function
 weights = tf.ones([batch_size, sequence_length])
